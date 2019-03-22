b70fee22bc1d5f4f4adefd59f667c22929246b2f
James Tyner <services@jamestyner.com>
2019-03-21 21:12:52 -0700

    Node 2 - ORM lab

--- app.js
+++ app.js
@@ -1,14 +1,194 @@
+const sqlite3 = require('sqlite3').verbose();
+
+let express = require("express");
+let app = express();
+
+// Sequelize ORM lab
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const Sequelize = require('sequelize');
+
+const Playlist = require("./models/playlist");
+const Artist = require("./models/artist");
+const Album = require("./models/album");
+const Track = require("./models/track");
+const Media_Type = require("./models/media-type");
+const Genre = require("./models/genre");
+
+
+// defining table relationships
+Artist.hasMany(Album, {
+  foreignKey:"ArtistId"
+});
+
+Album.belongsTo(Artist, {
+  foreignKey:"ArtistId"
+});
+
+Playlist.belongsToMany(Track, {
+  through:'playlist_track', //the join table
+  foreignKey:'PlaylistId',
+  timestamps:false // playlist_track has no timestamps
+});
+
+Track.belongsToMany(Playlist, {
+  through:"playlist_track",
+  foreignKey:"TrackId",
+  timestamps:false
+});
+
+Track.belongsTo(Album, {
+  foreignKey:"AlbumId"
+});
+
+Track.hasOne(Media_Type, {
+  foreignKey:"MediaTypeId"
+});
+
+Track.hasOne(Genre, {
+  foreignKey:"GenreId"
+});
+
+
+app.patch("/tracks/:id", function(request, response){
+  Track.findByPk(request.params.id, {
+    include: [Genre, Media_Type, Album]
+  }).then(function(track){
+    if(track){
+      let changes = request.body;
+      track.update(changes).then(function(updatedTrack){
+        response.json(updatedTrack);
+      }, function(errorResponse){
+        response.status(422).json({
+          errors:errorResponse.errors.map(function(error){
+            return {
+              attribute:error.path,
+              message:error.message
+            }
+          })
+        });
+      });
+    } else {
+      response.json(404);
+    }
+  });
+});
+
+app.delete("/api/playlists/:id", function(request, response){
+  let { id } = request.params;
+  Playlist.findByPk(id).then(function(playlist){
+    if (playlist){
+      return playlist.setTracks([]).then(function(){
+        return playlist.destroy();
+      });
+    } else {
+      return Promise.reject();
+    }
+  }).then(function(){
+    response.status(204).send();
+  }, function(){
+    response.status(404).send();
+  })
+});
+
+app.post("/api/artists", function(request, response){
+  Artist.create({
+    name:request.body.name
+  }).then(function(createdArtist){
+    response.json(createdArtist)
+  }, function(errorResponse){
+    response.status(422).json({
+      errors:errorResponse.errors.map(function(error){
+        return {
+          attribute:error.path,
+          message:error.message
+        };
+      })
+    });
+  });
+});
+
+app.get("/api/playlists", function(request, response){
+  // allowing URL queries
+  let filter = {}
+  let { q } = request.query;
+  if (q){
+    filter = {
+      where: {
+        name: {
+          [Sequelize.Op.like]:`${q}%`
+        }
+      }
+    }
+  }
+
+  Playlist.findAll(filter).then(function(playlists){
+    response.json(playlists);
+  });
+});
+
+app.get("/api/playlists/:id", function(request, response){
+  Playlist.findByPk(request.params.id, {
+    include: [Track]
+  }).then(function(result){
+    if (result){
+      response.json(result);
+    } else {
+      response.status(404).send();
+    }
+
+  });
+});
+
+app.get("/api/tracks/:id", function(request, response){
+  let { id } = request.params;
+  Track.findByPk(id, {
+    include: [Playlist]
+  }).then(function(result){
+    if (result){
+      response.json(result);
+    } else {
+      response.status(404).send();
+    }
+  });
+});
+
+app.get("/api/artists/:id", function(request, response){
+  let { id } = request.params;
+  Artist.findByPk(id, {
+    include:[Album]
+  }).then(function(result){
+    if (result){
+      response.json(result);
+    } else {
+      response.status(404).send();
+    }
+  })
+})
+
+app.get("/api/albums/:id", function(request, response){
+  let { id } = request.params;
+  Album.findByPk(id, {
+    include:[Artist]
+  }).then(function(result){
+    if (result){
+      response.json(result)
+    } else {
+      response.status(404).send();
+    }
+  })
+});
+
 // WebSockets assignment
 let WebSocket = require('ws');
 
-// create WebSocket server
 let wss = new WebSocket.Server({
   port:process.env.PORT || 8080
 });
 
-wss.on("connection", function(ws){ // this is for one connection instance
+wss.on("connection", function(ws){
   ws.on("message", function(message){
-    // wss.clients is the list of all connected clients on one server
     wss.clients.forEach((client) => {
       client.send(message);
     });
@@ -16,13 +196,6 @@ wss.on("connection", function(ws){ // this is for one connection instance
 });
 
 /* PREVIOUS ASSIGNMENT
-
-const sqlite3 = require('sqlite3').verbose();
-
-let express = require("express");
-let app = express();
-
-
 app.get('/api/artists', function(request, response){
   let db = new sqlite3.Database('chinook.db', (err) => {
     if (err) {
@@ -55,6 +228,6 @@ app.get('/api/artists', function(request, response){
       return console.error(err.message);
     }
   });
-});
+}); */
 
-app.listen(process.env.PORT || 8000); */
+app.listen(process.env.PORT || 8000);
